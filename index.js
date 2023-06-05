b6c12fe9ea8c230793fdb86d48c9fcd9b9a7344a
Milagros <miliguillot12@gmail.com>
2023-06-05 19:57:07 -0300

    primero

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
-import React from 'react'
+function laCajaDePandora(numero){
 
-const index = () => {
-  return (
-    <div>index</div>
-  )
+  if (numero % 2 === 0) {
+    // Convertir número par a binario
+    return numero.toString(2);
+  } else {
+    // Convertir número impar a hexadecimal
+    return numero.toString(16);
+  }
 }
 
-export default index
+// Ejemplo de uso
+const numeroEntero = parseInt(prompt("Ingrese un número entero:"));
+const resultado = convertirNumero(numeroEntero);
+console.log("El resultado es:", resultado);
+
+
+
