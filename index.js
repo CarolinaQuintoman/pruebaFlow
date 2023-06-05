974aecdfa1c11b83f9bef3c07b251ea2237e1b57
Bansant <lautaa26@gmail.com>
2023-06-05 19:39:30 -0300

    hice una funcion de la cajaa de pandora y declare una variable ... vi en un video que tiene que ser muy especifico lo que dice el commit para que la gente sepa claramente loque ponemos 😁

--- index.js
+++ index.js
@@ -1,9 +1,6 @@
-import React from 'react'
 
-const index = () => {
-  return (
-    <div>index</div>
-  )
+//Cada integrante en su rama debe modificar la función del punto número seis (6) para que reciba como parámetro un número entero. Si es un número par, debe convertirlo a binario y retornarlo, y si es impar, convertirlo a hexadecimal y retornarlo.
+const laCajaDePandora=(num)=>{
+  return number % 2 === 0 ? number.toString(2) : number.toString(16);
 }
-
-export default index
+let laCaja = "la resolvimos!"
