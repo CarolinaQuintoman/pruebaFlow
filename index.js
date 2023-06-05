ab7e4971c623e348a0eb1422945abbed6c636f58
VictorHugoRZ <v.zamoravictor@gmail.com>
2023-06-05 16:41:56 -0600

    Funcion Num

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
-import React from 'react'
 
-const index = () => {
-  return (
-    <div>index</div>
-  )
+const laCajaDePandora = (num) => {
+    if (num % 2 === 0) {
+      // Si es par, convertir a binario
+      return num.toString(2);
+    } else {
+      // Si es impar, convertir a hexadecimal
+      return num.toString(16);
+    }
 }
-
-export default index
